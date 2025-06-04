861c31ae846b7a9f7405e93d4e24538fda6c8a44
azifan <56614845+azifan@users.noreply.github.com>
2025-06-04 11:07:17 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,50 +1,66 @@
-import sys
-import pandas as pd
-from PyQt6.QtWidgets import QApplication, QWidget
 
-from src.CeusTool3d.selectImage_ui_helper import SelectImageGUI_CeusTool3d
-from src.CeusMcTool2d.selectImage_ui_helper import SelectImageGUI_CeusMcTool2d
-from src.UtcTool2d.selectImage_ui_helper import SelectImageGUI_UtcTool2dIQ
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
 from welcome_ui import Ui_WelcomePage
 
+# Attempt to import helper pages; disable buttons if unavailable
+try:
+    from src.CeusTool3d.selectImage_ui_helper import SelectImageGUI_CeusTool3d
+except ImportError:
+    SelectImageGUI_CeusTool3d = None
+
+try:
+    from src.CeusMcTool2d.selectImage_ui_helper import SelectImageGUI_CeusMcTool2d
+except ImportError:
+    SelectImageGUI_CeusMcTool2d = None
+
+try:
+    from src.UtcTool2d.selectImage_ui_helper import SelectImageGUI_UtcTool2dIQ
+except ImportError:
+    SelectImageGUI_UtcTool2dIQ = None
 
 class WelcomeGui(Ui_WelcomePage, QWidget):
+    """
+    Main welcome GUI that allows navigation to different tools.
+    """
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.utc2dButton.clicked.connect(self.moveToUtc2d)
-        self.dceus3dButton.clicked.connect(self.moveToDceus3d)
-        self.dceus2dButton.clicked.connect(self.moveToDceusMc2d)
+
+        # Connect buttons only if the corresponding class is available; else disable
+        if SelectImageGUI_UtcTool2dIQ:
+            self.utc2dButton.clicked.connect(lambda: self.open_page(SelectImageGUI_UtcTool2dIQ))
+        else:
+            self.utc2dButton.setEnabled(False)
+
+        if SelectImageGUI_CeusTool3d:
+            self.dceus3dButton.clicked.connect(lambda: self.open_page(SelectImageGUI_CeusTool3d))
+        else:
+            self.dceus3dButton.setEnabled(False)
+
+        if SelectImageGUI_CeusMcTool2d:
+            self.dceus2dButton.clicked.connect(lambda: self.open_page(SelectImageGUI_CeusMcTool2d))
+        else:
+            self.dceus2dButton.setEnabled(False)
+
         self.nextPage = None
         self.setLayout(self.verticalLayout)
 
-    def moveToUtc2d(self):
-        del self.nextPage
-        self.nextPage = SelectImageGUI_UtcTool2dIQ()
-        self.nextPage.show()
-        self.nextPage.resize(self.size())
-        self.nextPage.welcomeGui = self
-        self.hide()
+    def open_page(self, PageClass):
+        """
+        Open the next page GUI by instantiating the given PageClass.
+        """
+        try:
+            self.nextPage = PageClass()
+            self.nextPage.resize(self.size())
+            self.nextPage.welcomeGui = self
+            self.nextPage.show()
+            self.hide()
+        except Exception as e:
+            print(f"Error opening page: {e}")
 
-    def moveToDceus3d(self):
-        del self.nextPage
-        self.nextPage = SelectImageGUI_CeusTool3d()
-        self.nextPage.show()
-        self.nextPage.resize(self.size())
-        self.nextPage.welcomeGui = self
-        self.hide()
-
-    def moveToDceusMc2d(self):
-        del self.nextPage
-        self.nextPage = SelectImageGUI_CeusMcTool2d()
-        self.nextPage.show()
-        self.nextPage.welcomeGui = self
-        self.hide()
-
-
-# -----------------------------------------------------------------------------------------------
 if __name__ == "__main__":
-    welcomeApp = QApplication(sys.argv)
-    welcomeUI = WelcomeGui()
-    welcomeUI.show()
-    sys.exit(welcomeApp.exec())
+    app = QApplication(sys.argv)
+    ui = WelcomeGui()
+    ui.show()
+    sys.exit(app.exec())
